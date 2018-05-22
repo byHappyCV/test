062dd00283de48c79c711f7ac7de1ddd589ebf97
Pasha Rychkov <rychkovcv@gmail.com>
2018-05-22 10:02:51 +0300

    1

--- manoliydodik/Program.cs
+++ manoliydodik/Program.cs
@@ -6,7 +6,7 @@ namespace manoliydodik
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("manolydodik");
         }
     }
 }
